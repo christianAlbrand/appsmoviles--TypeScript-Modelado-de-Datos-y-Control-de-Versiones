5460f5481db627edaf16daf048fd2607665a07d4
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-06-17 19:46:27 -0700

    crear servicio para usar alert al agregar una tarea

---
+++ src/app/services/alert.ts
@@ -0,0 +1,50 @@
+import { inject, Injectable } from '@angular/core';
+import { AlertController, IonButton } from '@ionic/angular/standalone';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Alert {
+  private alertController: AlertController = inject(AlertController);
+
+  async showAlert(headerText: string, messageText: string) {
+    const alert = await this.alertController.create({
+      header: headerText,
+      message: messageText,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async confirmAlert(
+    header: string,
+    message: string,
+    funcionOk: Function,
+    cancelText: string = 'Cancelar',
+    confirmText: string = 'Aceptar'
+  )
+
+  {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: cancelText,
+          role: 'cancel',
+        },
+        {
+          text: confirmText,
+          role: 'confirm',
+          handler: () => {
+            funcionOk();
+          }
+        }
+      ],
+    });
+
+    await alert.present();
+  }
+}
